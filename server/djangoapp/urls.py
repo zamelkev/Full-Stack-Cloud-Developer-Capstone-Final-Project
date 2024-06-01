439460e2df34338030b567e11f5788006ca57245
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-06-01 13:29:40 +0200

    3: Build CarModel and CarMake Django Models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -19,4 +19,7 @@ urlpatterns = [
 
     # path for add a review view
 
+    # path - get cars
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,5 +1,3 @@
-# Uncomment the required imports before adding the code
-
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
@@ -14,6 +12,8 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
+
+from .models import CarMake, CarModel
 
 
 # Get an instance of a logger
@@ -79,6 +79,18 @@ def registration(request):
         data = {"userName":username,"error":"Already Registered"}
         return JsonResponse(data)
 # ...
+
+#### Get Cars ####
+def get_cars(request):
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
