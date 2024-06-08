e748e8a472ba905d9c70141c9b577bf22d770684
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-06-08 10:10:01 +0200

    adding features - fixing issues / 5: Add dynamic pages

--- server/database/app.js
+++ server/database/app.js
@@ -57,18 +57,33 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 });
 
 // Express route to fetch all dealerships
-app.get('/fetchDealers', async (req, res) => {
-//Write your code here
+app.get("/fetchDealers", async (req, res) => {
+  try {
+    const documents = await Dealerships.find();
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching dealerships" });
+  }
 });
 
 // Express route to fetch Dealers by a particular state
-app.get('/fetchDealers/:state', async (req, res) => {
-//Write your code here
+app.get("/fetchDealers/:state", async (req, res) => {
+  try {
+    const documents = await Dealerships.find({ state: req.params.state });
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching dealerships" });
+  }
 });
 
 // Express route to fetch dealer by a particular id
-app.get('/fetchDealer/:id', async (req, res) => {
-//Write your code here
+app.get("/fetchDealer/:id", async (req, res) => {
+  try {
+    const documents = await Dealerships.find({ id: req.params.id });
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching dealerships" });
+  }
 });
 
 //Express route to insert review
