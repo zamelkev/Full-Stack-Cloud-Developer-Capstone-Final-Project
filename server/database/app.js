657ce7daf66800fbeb01be594370f131a32dc5d5
Kevin Zamora <kevin.zamora.amela@gmail.com>
2024-06-09 10:23:44 +0200

    in progress / fixing lint javascript convention issues

--- server/database/app.js
+++ server/database/app.js
@@ -1,11 +1,12 @@
+/*eslint dot-notation: ["error", { "allowKeywords": false }]*/
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -33,7 +34,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -89,8 +90,8 @@ app.get("/fetchDealer/:id", async (req, res) => {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id']+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0]['id']+1;
 
   const review = new Reviews({
 		"id": new_id,
