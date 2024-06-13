0548c79d45c7f779aea47061a130c442c6ee4dbd
Kevin Zamora <56316102+zamelkev@users.noreply.github.com>
2024-06-13 10:31:24 +0200

    Update urls.py / fixing execution issue

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -33,5 +33,4 @@ urlpatterns = [
     path('postreview/<int:dealer_id>',
          TemplateView.as_view(template_name="index.html")),
     path('', TemplateView.as_view(template_name="Home.html")),
-]
-+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
